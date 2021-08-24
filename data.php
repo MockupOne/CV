ce09684c7c816e4a33cd9863d026c54c141e19b8
MockupOne <78219754+MockupOne@users.noreply.github.com>
2021-08-24 13:18:12 +0300

    Add files via upload

---
+++ data.php
@@ -0,0 +1,84 @@
+<?php
+
+return [
+    'about' => [
+        'name' => 'Dmitry Kaskevich',
+        'post' => 'Junior PHP-developer',
+        'email' => 'dmkacke@gmail.com',
+        'phone' => '+375259115405',
+        'site' => 'https://mockupone.github.io/Resume/'
+    ],
+    'education' =>
+    [
+        [
+        'faculty' => 'Курс PHP',
+        'university' => 'Glo academy',
+        'yearStart'=> 2021,
+        'yearEnd'=>2021
+        ],
+        [
+        'faculty' => 'Web разработка  с нуля до профессионала. Полный курс',
+        'university' => 'Udemy',
+        'yearStart'=> 2020,
+        'yearEnd'=>2021
+        ]
+    ],
+    'languages'=>['English','Russian','Belarusian'],
+    'interests'=>['Table tennis', 'Drowing'],
+    'aboutCareer'=>'Я начинающий веб-разработчик. С 2020 года изучаю веб-программирование. Знаю HTML, CSS, JS, JQuery, Bootstrap. Данный сайт является учебным проектам по PHP. Я уверен, что многого ещё не знаю, однако мне нравится учиться, я умею пользоваться гуглом, поэтому всегда смогу найти ответ на тот вопрос, который встанет передо мной.',
+    'experiences' =>[
+        [
+            'post'=>'Специалист по работе с клиентами',
+            'place'=>'BSL-Contact',
+            'duty'=>'Работа в колл-центре',
+            'yearStart'=> 2020,
+            'yearEnd'=>2021
+        ],
+        [
+            'post'=>'Специалист по работе с малым и средним бизнесом',
+            'place'=>'ОАО "Белагропромбанк',
+            'duty'=>'Сфера продаж в B2B',
+            'yearStart'=> 2019,
+            'yearEnd'=>2020
+        ],
+        [
+            'post'=>'Продавец межрозничной торговли',
+            'place'=>'ОАО "Молочный Мир"',
+            'duty'=>'Продажа мороженого',
+            'yearStart'=> 2013,
+            'yearEnd'=>2013
+        ]
+    ],
+    'projects'=>[
+        [
+            'title'=>'Мой сайт-портфолио',
+            'link'=>'https://mockupone.github.io/Resume/',
+            'about'=>'Данный сайт является сборником моих проектов, листая слайдер которого, можно с ними познакомиться'
+        ],
+        [
+            'title'=>'Учебный проект - Willberries',
+            'link'=>'https://mockupone.github.io/Willberries/',
+            'about'=>'В данном проекте демонстрируются навыки вёрстки и знание библиотек'
+        ]
+    ],
+    'skills'=>[
+        [
+            'title'=>'PHP & Ajax',
+            'level'=>'40'
+        ],
+        [
+            'title'=>'Javascript & jQuery',
+            'level'=>'40'
+        ],
+        [
+            'title'=>'HTML5 & CSS',
+            'level'=>'80'
+        ],
+        [
+            'title'=>'Figma & Photoshop',
+            'level'=>'60'
+        ]
+
+
+    ]
+];
